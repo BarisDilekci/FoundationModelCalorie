485c5397884899dea35e25cb326c4152bcd997a2
barisdilekci <barixmenuygulama@gmail.com>
2025-12-27 22:49:20 +0300

    Initial Commit

---
+++ FoundationModelCalorie/FoundationModelCalorieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelCalorieApp.swift
+//  FoundationModelCalorie
+//
+//  Created by Barış Dilekçi on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelCalorieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
